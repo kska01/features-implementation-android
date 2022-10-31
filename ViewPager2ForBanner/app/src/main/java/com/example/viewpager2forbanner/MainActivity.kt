a20839e692c5966e4a52e1a399d2fb11164cd770
ka <insideka01@gmail.com>
2022-10-31 10:41:54 +0900

    10.31 배너 형식과 같이 하단 오른쪽에 페이지수 표시하기

--- ViewPager2ForBanner/app/src/main/java/com/example/viewpager2forbanner/MainActivity.kt
+++ ViewPager2ForBanner/app/src/main/java/com/example/viewpager2forbanner/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.viewpager2forbanner
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
 import com.example.viewpager2forbanner.data.DataPage
 import com.example.viewpager2forbanner.databinding.ActivityMainBinding
@@ -19,8 +19,8 @@ class MainActivity : AppCompatActivity() {
         val list: ArrayList<DataPage> = ArrayList<DataPage>().let {
             it.apply {
                 add(DataPage(android.R.color.holo_red_light, "1 Page"))
-                add(DataPage(android.R.color.holo_orange_dark, "2 Page"))
-                add(DataPage(android.R.color.holo_green_dark, "3 Page"))
+                add(DataPage(android.R.color.holo_orange_light, "2 Page"))
+                add(DataPage(android.R.color.holo_green_light, "3 Page"))
                 add(DataPage(android.R.color.holo_blue_bright, "4 Page"))
                 add(DataPage(android.R.color.holo_purple, "5 Page"))
                 add(DataPage(android.R.color.darker_gray, "6 Page"))
@@ -29,5 +29,15 @@ class MainActivity : AppCompatActivity() {
 
         binding.viewPager.adapter = ViewPagerAdapter(list)
         binding.viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+        // banner 의 숫자 설정
+        binding.txtCurrentBanner.text = getString(R.string.viewpager2_banner, 1, list.size)
+        // banner 를 드래그해서 넘겼을 때 포지션 값을 변경해주는 callBack 함수 등록
+        binding.viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                binding.txtCurrentBanner.text =
+                    getString(R.string.viewpager2_banner, position + 1, list.size)
+            }
+        })
     }
 }
