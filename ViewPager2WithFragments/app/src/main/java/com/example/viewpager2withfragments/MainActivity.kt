dc5c47d4fba93bfd5225c339a4af6e9e3a1904e0
ka <insideka01@gmail.com>
2022-12-14 17:53:03 +0900

    12.14

---
+++ ViewPager2WithFragments/app/src/main/java/com/example/viewpager2withfragments/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.viewpager2withfragments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
